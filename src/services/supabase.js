0646cb961884168ba96f6b07afb0f3bc39fc2c96
hlibit <hlibm2001@gmail.com>
2025-01-17 17:28:42 +0200

    v1.1.0
    supabase connected

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://bvhsltkmmujhblwonrxm.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2aHNsdGttbXVqaGJsd29ucnhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxMTc3NjMsImV4cCI6MjA1MjY5Mzc2M30.-FsT8SHK-LhkCsN7xcAZlaKelZOJFi21ipFI1LyYUVE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
